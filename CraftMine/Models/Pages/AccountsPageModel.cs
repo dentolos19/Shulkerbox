417299d47811ff99470265abeada5226c1beca5d
Dennise Catolos <dentolos19@gmail.com>
2022-09-07 16:14:07 +0800

    Fixed another set of issues

--- CraftMine/Models/Pages/AccountsPageModel.cs
+++ CraftMine/Models/Pages/AccountsPageModel.cs
@@ -12,6 +12,8 @@ namespace CraftMine.Models;
 public partial class AccountsPageModel : ObservableObject
 {
 
+    [ObservableProperty] private string _username = string.Empty;
+    [ObservableProperty] private AccountItemModel? _account;
     [ObservableProperty] private ObservableCollection<AccountItemModel> _accounts = new();
 
     public AccountsPageModel()
@@ -20,41 +22,41 @@ public partial class AccountsPageModel : ObservableObject
     }
 
     [RelayCommand]
-    private async Task Add(string username)
+    private async Task Add()
     {
-        if (string.IsNullOrEmpty(username))
-        {
-            await App.AttachDialog("Enter a username before adding.", "Halt!");
-            return;
-        }
-        if (SettingsService.Instance.Accounts.Contains(username, StringComparer.OrdinalIgnoreCase))
-        {
-            await App.AttachDialog("Your username already exists.", "Halt!");
-            return;
-        }
-        if (!Regex.IsMatch(username, "^[a-zA-Z0-9_]{2,16}$"))
+        if (!Regex.IsMatch(Username, "^[a-zA-Z0-9_]{2,16}$"))
         {
             await App.AttachDialog("Your username is invalid.", "Halt!");
             return;
         }
+        if (SettingsService.Instance.Accounts.Contains(Username, StringComparer.OrdinalIgnoreCase))
+        {
+            await App.AttachDialog("Your username already exists.", "Halt!");
+            return;
+        }
         var accounts = SettingsService.Instance.Accounts.ToList();
-        accounts.Add(username);
+        accounts.Add(Username);
         SettingsService.Instance.Accounts = accounts.ToArray();
         await RefreshCommand.ExecuteAsync(null);
     }
 
     [RelayCommand]
-    private async Task Remove(AccountItemModel item)
+    private async Task Remove()
     {
+        if (Account is null)
+        {
+            await App.AttachDialog("Select an account to remove.", "Halt!");
+            return;
+        }
         if (Accounts.Count <= 1)
         {
             await App.AttachDialog("You must at least have one account available, therefore you can't remove this account.", "Halt!");
             return;
         }
-        if (SettingsService.Instance.Accounts.Contains(item.Username, StringComparer.OrdinalIgnoreCase))
+        if (SettingsService.Instance.Accounts.Contains(Account.Username, StringComparer.OrdinalIgnoreCase))
         {
             var accounts = SettingsService.Instance.Accounts.ToList();
-            accounts.Remove(item.Username);
+            accounts.Remove(Account.Username);
             SettingsService.Instance.Accounts = accounts.ToArray();
         }
         await RefreshCommand.ExecuteAsync(null);
