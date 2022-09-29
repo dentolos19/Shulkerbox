bae8c3e379ca4458b040476d0ce1f9f80c87d7fb
Dennise Catolos <dentolos19@gmail.com>
2022-09-29 16:50:06 +0800

    Revamped account creation functionality

---
+++ CraftMine/Dialogs/OfflineLoginDialog.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace CraftMine.Dialogs;
+
+public sealed partial class OfflineLoginDialog
+{
+
+    public string? Username { get; private set; }
+
+    public OfflineLoginDialog()
+    {
+        InitializeComponent();
+    }
+
+    private void OnPrimaryButtonClicked(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+    {
+        Username = UsernameBox.Text;
+    }
+
+}
--- CraftMine/Models/Pages/AccountsPageModel.cs
+++ CraftMine/Models/Pages/AccountsPageModel.cs
@@ -6,21 +6,39 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CraftMine.Dialogs;
 using CraftMine.Services;
+using Microsoft.UI.Xaml.Controls;
 
 namespace CraftMine.Models;
 
 public partial class AccountsPageModel : ObservableObject
 {
 
-    [ObservableProperty] private string _username = string.Empty;
     [ObservableProperty] private AccountItemModel? _account;
     [ObservableProperty] private ObservableCollection<AccountItemModel> _accounts = new();
 
     [RelayCommand]
-    private async Task Add()
+    private async Task AddMicrosoft()
     {
-        if (!Regex.IsMatch(Username, "^[a-zA-Z0-9_]{2,16}$"))
+        await App.AttachDialog("This feature is not implemented yet!", "Halt!");
+    }
+
+    [RelayCommand]
+    private async Task AddMojang()
+    {
+        await App.AttachDialog("This feature is not implemented yet!", "Halt!");
+    }
+
+    [RelayCommand]
+    private async Task AddOffline()
+    {
+        var dialog = new OfflineLoginDialog();
+        if (await App.AttachDialog(dialog) != ContentDialogResult.Primary)
+            return;
+        if (dialog.Username is null)
+            return;
+        if (!Regex.IsMatch(dialog.Username, "^[a-zA-Z0-9_]{2,16}$"))
         {
             await App.AttachDialog("Your username is invalid.", "Halt!");
             return;
@@ -28,12 +46,12 @@ public partial class AccountsPageModel : ObservableObject
         var accounts = new List<string>();
         if (SettingsService.Instance.Accounts is { Length: > 0 })
             accounts = SettingsService.Instance.Accounts.ToList();
-        if (accounts.Contains(Username, StringComparer.OrdinalIgnoreCase))
+        if (accounts.Contains(dialog.Username, StringComparer.OrdinalIgnoreCase))
         {
             await App.AttachDialog("Your username already exists.", "Halt!");
             return;
         }
-        accounts.Add(Username);
+        accounts.Add(dialog.Username);
         SettingsService.Instance.Accounts = accounts.ToArray();
         await RefreshCommand.ExecuteAsync(null);
     }
